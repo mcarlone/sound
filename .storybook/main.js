287e18e74f36d56a1a61a517adbcb47fcde9339f
Matt Carlone <mcarlone@Matt-Carlone.local>
2020-05-06 15:32:56 -0400

    fart hands

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  stories: ['../src/stories/demos/*.stories.js'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-storysource',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-links',
+  ],
+};
---
+++ src/stories/demos/2-PlaySound.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import useSound from 'use-sound';
+
+import fart1Sfx from '../sounds/fart-2.mp3';
+import fart2Sfx from '../sounds/fart-3.mp3';
+
+import fartHandsOff from '../img/fart-hands-off.png';
+import fartHandsOn from '../img/fart-hands-on.png';
+
+export default { title: 'Fart Hands' };
+
+
+
+
+export const FartHands = () => {
+  const [isDown, setIsDown] = React.useState(false);
+
+  const [playFart1] = useSound(fart1Sfx, {playbackRate:1.0 + Math.random() * 1.5});
+  const [playFart2] = useSound(fart2Sfx, {playbackRate:1.0 + Math.random() * 1.5});
+
+  const onMouseDown = () => { setIsDown(true); playFart1(); };
+  const onMouseUp = () => { setIsDown(false); playFart2(); }
+
+  return <img src={isDown ? fartHandsOn : fartHandsOff} onMouseDown={onMouseDown} onMouseUp={onMouseUp}/>
+}
+
+
+export const BasicPlayback = () => {
+  return (<div>Hello!<FartHands/></div>);
+};
+
